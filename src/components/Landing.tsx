cbde512e2e4cc6a9914c037b969cadafe6dac967
zapboy216 <zapboy216@gmail.com>
2024-08-01 13:45:14 -0400

    reverted to previous state, correct errors

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -3,7 +3,8 @@
 const Landing = () => {
   return (
     <section id="landing" className="min-h-screen flex items-center justify-center bg-gray-100">
-     <h1>Welcome Home</h1>
+           <h1 className="text-4xl animate__animated animate__fadeIn">Welcome Home</h1>
+
     </section>
   );
 };
